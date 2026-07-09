f16f173b654946c721506940c71d4a752c4d1ff0
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-07-09 23:27:19 +0530

    Fix import statement formatting in MenuPage.jsx

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,4 +1,4 @@
-;import { useState, useEffect } from "react"; 
+import { useState, useEffect } from "react"; 
 import { useCart } from "../context/CartContext";
 import { db } from "../firebase"; // Ensure your firebase connection is exported here
 import { collection, getDocs } from "firebase/firestore";
