bd91209ddf78d9d61b9562e48edc305bd1abab64
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-07-09 22:11:14 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // 1. Add this import
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZXXMZOvmUviZqgDoljAhSllaQLxelvfY",
-    authDomain: "brewed-238a8.firebaseapp.com",
-      projectId: "brewed-238a8",
-        storageBucket: "brewed-238a8.firebasestorage.app",
-          messagingSenderId: "488530129853",
-            appId: "1:488530129853:web:c737eae789215cabc318d4",
-            };
+  apiKey: "AizSyAZXXMZOvmUviZqgDoljAhSllaQLxelvfY",
+  authDomain: "brewed-238a8.firebaseapp.com",
+  projectId: "brewed-238a8",
+  storageBucket: "brewed-238a8.firebasestorage.app",
+  messagingSenderId: "488530129853",
+  appId: "1:488530129853:web:c737eae789215cabc318d4"
+};
 
-            const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-            export const auth = getAuth(app);
-            export const googleProvider = new GoogleAuthProvider();
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app); // 2. Add this line to export the database
