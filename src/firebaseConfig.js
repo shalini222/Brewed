1f19d421c8f2c036ed71d69f987a300f59a2ae72
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-07-09 22:04:55 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,1 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  // PASTE YOUR FIREBASE CONFIG OBJECT HERE
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
