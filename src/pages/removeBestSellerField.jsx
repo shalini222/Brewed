b13cb05aa666841db4e4c87d22dedb0eada85a5f
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-07-19 12:34:33 +0530

    Remove Best Seller field from MigrateMenu

---
+++ src/pages/removeBestSellerField.jsx
@@ -0,0 +1,17 @@
+import { collection, getDocs, updateDoc, deleteField } from "firebase/firestore";
+import { db } from "./firebase";
+
+async function removeBestSellerField() {
+  const snapshot = await getDocs(collection(db, "menu"));
+
+  for (const document of snapshot.docs) {
+    await updateDoc(document.ref, {
+      isBestSeller: deleteField(),
+    });
+    console.log(`Updated ${document.id}`);
+  }
+
+  console.log("Finished!");
+}
+
+removeBestSellerField();
