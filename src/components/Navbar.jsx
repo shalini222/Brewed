fb403e11d4a3599f5c8685f3a25f46f88791b460
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-07-21 21:15:38 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -266,6 +266,12 @@ export default function HeadlessNavbar({ currentPage, setPage }) {
                   <span>⭐</span> Rewards
                 </button>
 
+
+                <button
+onClick={()=>setPage("address")}
+>
+<span>🏠</span>My Addresses
+</button>
                 
 
                 <button onClick={() => { setShowMenu(false); setPage("settings"); }}>
