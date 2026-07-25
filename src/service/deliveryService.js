53ee274e1487d399cfb6926aad901ae77bbd3fb9
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-07-26 03:22:09 +0530

    Update deliveryService.js

--- src/service/deliveryService.js
+++ src/service/deliveryService.js
@@ -1,5 +1,5 @@
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../firebase.js";
 
 export async function checkDelivery(pincode) {
   try {
