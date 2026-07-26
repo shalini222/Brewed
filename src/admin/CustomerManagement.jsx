4f5de78445a369e37b3b70be8a9ccb35c96c39af
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-07-26 21:05:19 +0530

    Update CustomerManagement.jsx

--- src/admin/CustomerManagement.jsx
+++ src/admin/CustomerManagement.jsx
@@ -1,1 +1,14 @@
-Customer
+import React from 'react';
+
+
+function CustomerManagement({setPage}) {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>cutomer page</h1>
+      <p>This is a minimal boilerplate for a React application.</p>
+    </div>
+  );
+}
+
+
+
