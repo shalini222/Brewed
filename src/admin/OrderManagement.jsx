ccb32fab6d544e1953da2c79b0051c3984c0b791
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-07-27 19:00:28 +0530

    Update OrderManagement.jsx

--- src/admin/OrderManagement.jsx
+++ src/admin/OrderManagement.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function OrderManagement ({ setPage}){
+export default function OrderManagement ({ setPage, setActivePage}){
   return(
     <h1>helloo</h1>
 
