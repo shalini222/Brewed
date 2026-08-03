1d706dbefd1f74752037ead34c66901097cad9e6
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-08-03 18:22:35 +0530

    Update SupportPage component state management
    Refactor SupportPage to manage selectedTicket and activePage states.

--- src/pages/SupportPage.jsx
+++ src/pages/SupportPage.jsx
@@ -14,10 +14,12 @@ import {
 } from "firebase/firestore";
 import { Send, CheckCheck, Eye, Lock, FileText, UserCheck, MessageSquarePlus } from "lucide-react";
 
-export default function SupportPage({ setPage, setActivePage, selectedTicket }) {
+export default function SupportPage({ setPage  }) {
   const [messages, setMessages] = useState([]);
   const [reply, setReply] = useState("");
   const [sending, setSending] = useState(false);
+  const [selectedTicket, setSelectedTicket] = useState(null);
+  const [activePage, setActivePage] = useState("menu");
 
   const containerRef = useRef(null);
   const messagesEndRef = useRef(null);
