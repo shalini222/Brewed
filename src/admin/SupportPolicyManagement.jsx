b4cabfadfe19fbd0cbaa99525fabc4bba317746b
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-08-04 02:10:15 +0530

    Remove unused props from SupportPolicyManagement

--- src/admin/SupportPolicyManagement.jsx
+++ src/admin/SupportPolicyManagement.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function SupportPolicyManagement({ setPage , setActivePage }){
+export default function SupportPolicyManagement(){
   return(
 
     <h1>helloooo Policy</h1>
