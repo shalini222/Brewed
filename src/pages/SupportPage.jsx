20e98e5011fbd4d38aa172fad11b885796119d2e
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-08-03 19:12:06 +0530

    Update SupportPage.jsx

--- src/pages/SupportPage.jsx
+++ src/pages/SupportPage.jsx
@@ -66,7 +66,7 @@ export default function SupportPage({ setPage }) {
     const q = query(
       collection(db, "supportTickets"),
       where("customerId", "==", currentUser.uid),
-      orderBy("updatedAt", "desc")
+    
     );
 
     const unsubscribe = onSnapshot(
