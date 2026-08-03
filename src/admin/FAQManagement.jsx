1aef9dec8a67d65c05238d1930c94304157ddcda
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-08-04 01:51:07 +0530

    Update and rename FAQManagement.jsx to SupportFAQManagement.jsx

--- src/admin/FAQManagement.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FAQManagement ({ setPage , setActivePage }){
-  return(
-
-    <h1>helloooo faqss</h1>
-);
-
-}
---
+++ src/admin/SupportFAQManagement.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SupportFAQManagement ({ setPage , setActivePage }){
+  return(
+
+    <h1>helloooo faqss</h1>
+);
+
+}
