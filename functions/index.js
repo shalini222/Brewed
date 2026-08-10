593d5b02a203dba8b6c0f4c756eb638d9c1d7003
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-08-10 13:52:01 +0530

    Replace Razorpay secret keys with hardcoded values
    Replaced secret keys with hardcoded Razorpay credentials.

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,8 @@ setGlobalOptions({
   maxInstances: 10,
 });
 
-const razorpayKeyId = defineSecret("RAZORPAY_KEY_ID");
-const razorpayKeySecret = defineSecret("RAZORPAY_KEY_SECRET");
+const razorpayKeyId = "rzp_test_TMzJBC3B2XTAoq";
+const razorpayKeySecret = "Cs743CL6dgBo4QYf0PUFxlsC"; 
 
 exports.createRazorpayOrder = onCall(
   {
