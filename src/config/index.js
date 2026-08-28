11eb2c9af072ec2dfea31cd0a42a360bf479067b
shalini222 <72884585+shalini222@users.noreply.github.com>
2026-08-28 21:08:58 +0530

    Add index.js to export configuration modules

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export { default as brand } from "./brand";
+export { default as theme } from "./theme";
+export { default as business } from "./business";
+export { default as features } from "./features";
